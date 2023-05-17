7cd1c074f0c741ea3b3210b4e1b2342ff6f33ee4
anhnguyen-coder <anhndgch210594@fpt.edu.vn>
2023-05-17 17:08:45 +0700

    my_first_website

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+
+//render ra form input
+router.get('/', (req, res) =>{
+  res.render('input')
+})
+
+//2. lay du lieu tut form input
+router.post('/output', (req, res) =>{  
+  var body = req.body;
+
+  // var name =  req.body.ten;
+  // var age = req.body.tuoi;
+  // var email = req.body.email;
+  // var DOB = req.body.ngaysinh;
+
+  // A. In ra console log
+  // console.log(req.body);
+  // B. In ra web voi res.send
+  // res.send(req.body);
+  //C. render ra trang output kem du lieu tu form
+  // res.render('output',{thanthe: body})
+  res.render('output',{thanthe: body})
+})
+
+router.get('/login', (req, res) =>{
+  res.render('login')
+})
+
+router.post('/check', (req, res) =>{
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if(username =='admin' && password == '123456'){
+    res.send("<h1 style='color:green; background-color:greenyellow;'>Login successfully !!!</h1>");
+  }else{
+    res.send("<h1 style='color:red; background-color:yellow;'>Login failed !!!</h1>");
+  }
+})
+module.exports = router;
